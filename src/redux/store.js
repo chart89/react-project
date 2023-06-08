de6a7bb8ec9a54b69bad892e76087a2b096fd141
Darek Biernat <kaiju83@outlook.com>
2023-06-08 23:10:20 +0200

    Add selectors and create folder 'utils' with global functions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,14 @@
 import { createStore } from 'redux';
 import initialState from './initialState';
 import shortid from 'shortid';
+import strContains from '../utils/strContains';
+//selectors
+export const getFilteredCards = ({cards, searchString}, columnId) => cards
+  .filter(card => card.columnId === columnId && strContains(card.title, searchString));
+
+export const getAllColumns = state => state.columns;
+
+
 
 const reducer = (state, action) => {
 
